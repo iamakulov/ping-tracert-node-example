0aaa266aab1eb8af7c97cf6cf2783cf3289c2abc
Ivan Akulov <gxoptg@gmail.com>
2018-02-17 14:39:41 +0300

    Implement tracert

--- src/index.ts
+++ src/index.ts
@@ -11,35 +11,54 @@ const socket = raw.createSocket({
     addressFamily: raw.AddressFamily.IPv4,
 });
 
-console.log(`Pinging ${process.env.IP_ADDRESS}...`);
-let sequenceNumber = 0;
+console.log(`Tracing route to ${process.env.IP_ADDRESS}...`);
+let currentStep = 1;
 setInterval(() => {
+    const memoizedCurrentStep = currentStep;
     const buffer = createBufferFromPingMessage({
         identifier: 200,
-        sequenceNumber,
+        sequenceNumber: currentStep,
     });
+    socket.setOption(
+        raw.SocketLevel.IPPROTO_IP,
+        raw.SocketOption.IP_TTL,
+        currentStep,
+    );
     socket.send(
         buffer,
         0,
         buffer.length,
         process.env.IP_ADDRESS,
-        () => {},
+        () => {
+            socket.setOption(
+                raw.SocketLevel.IPPROTO_IP,
+                raw.SocketOption.IP_TTL,
+                memoizedCurrentStep,
+            );
+        },
         () => {},
     );
 
     socket.once('message', (buffer: Buffer, source: string) => {
-        const ipMessage = parseIpMessageFromBuffer(buffer).data;
-        const pingMessage = parsePingMessageFromBuffer(ipMessage);
-        console.log(
-            `Received ${buffer.length} bytes from ${source}. Identifier is ${
-                pingMessage.identifier
-            }, sequence number is ${pingMessage.sequenceNumber}`,
-        );
+        const ipMessage = parseIpMessageFromBuffer(buffer);
+        console.log(memoizedCurrentStep, source);
+
+        try {
+            parsePingMessageFromBuffer(ipMessage.data);
+            // If parsed successfully, then the ping message was returned => we reached the end
+            process.exit(0);
+        } catch (e) {
+            // Do nothing
+        }
     });
 
     socket.once('error', error => {
         console.log(error);
     });
 
-    ++sequenceNumber;
+    socket.once('close', error => {
+        console.log(error);
+    });
+
+    ++currentStep;
 }, 1000);
